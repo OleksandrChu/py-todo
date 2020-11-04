e86d6b5f025f66879606f8481bb5b67c78fc5c69
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-11-04 02:13:18 +0200

    Test

--- list/tests.py
+++ list/tests.py
@@ -13,3 +13,18 @@ class TodoListTest(TestCase):
     def test_todolist_creation(self):
         t = self.create_todolist()
         self.assertTrue(isinstance(t, TodoList))
+
+    def test_get_all_lists(self):
+        response = self.client.get('/list/', follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list/list.html')
+
+    # def test_update_todolist_title(self):
+    #     resp = self.client.post('/list/create/', {"title": "ex"})
+    #     print(resp)
+    #     response = self.client.put('/list/update/', {"id": "1", "title": "ex"})
+    #     self.assertEqual(response.status_code, 200)
+
+    # def test_call_view_fail_blank(self):
+    #     response = self.client.post('/list/create/', {})
+    #     self.assertFormError(response, 'form', 'title', 'This field is required.')
